089342f6c05f490fd321ecac59276532d482cf7d
kodamayuto <yuto2001gyokugan@gmail.com>
2021-05-11 01:13:11 +0900

    2021-0511-0113

--- main.c
+++ main.c
@@ -49,10 +49,59 @@ int binary_search(int arr[], int target,int arr_len) {
 	return -1;
 }
 
+void bubble_sort(int arr[],int arr_len) {
+	for (int i = 0; i < arr_len; i++)
+	{
+		for (int j = i+1; j < arr_len; j++)
+		{
+			if (arr[j] < arr[i]) {
+				int min = arr[j];
+				arr[j] = arr[i];
+				arr[i] = min;
+			}
+		}
+	}
+}
+
+void quick_sort(int arr[], int arr_len) {
+	int pivot = (int)(arr_len / 2);
+	int left = 0;
+	int right = arr_len - 1;
+	int lr_flag = 0;
+	while (1)
+	{
+		if (lr_flag == 0) {
+			//	左　基準以下の値なら
+			if (arr[left] <= arr[pivot]) {
+				//	入れ替える
+				arr[right] = arr[left];
+				//	左フラグを右フラグに
+				lr_flag = 1;
+			}
+			else { left++; }
+		}
+		else {
+			//	右　基準より上の値なら
+			if (arr[right] > arr[pivot]) {
+				//	入れ替える
+				arr[left] = arr[right];
+				//	右フラグを左フラグに
+				lr_flag = 0;
+			}
+			else { right--; }
+		}
+		//	大きいグループと小さいグループの境界線
+		if ((right - left) == 1) {
+			//	大きいグループ
+			//  小さいグループ
+		}
+	}
+}
+
 int main(void) {
 	// min(3, 5);
 	// max(3, 5);
-	int arr[10] = { 0,5,10,15,20,25,30,35,40,45 };
+	//int arr[10] = { 0,5,10,15,20,25,30,35,40,45 };
 	//swap(arr, 1, 9, 10);
 	//for (int i = 0; i < 10; i++)
 	//{
@@ -60,7 +109,14 @@ int main(void) {
 	//}
 	//int x = linear_search(arr, 5, 10);
 	//printf("%d\n", x);
-	int x = binary_search(arr, 1, 10);
-	printf("%d\n", x);
+	//int x = binary_search(arr, 1, 10);
+	//printf("%d\n", x);
+	int a[10] = { 51,23,75,40,34,79,79,82,31,0 };
+	//bubble_sort(a, 10);
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	printf("%d\n", a[i]);
+	//}
+
 	return 0;
 }
